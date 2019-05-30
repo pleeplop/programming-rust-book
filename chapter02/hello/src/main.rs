81e79c34dd5140a64d2c39640117731c4e222ec3
pleeplop <pleeplop@users.noreply.github.com>
2019-05-30 20:26:22 +0200

    chapter02

---
+++ chapter02/hello/src/main.rs
@@ -0,0 +1,24 @@
+use hello::*;
+use std::io::Write;
+use std::str::FromStr;
+
+fn main() {
+    let a = 48;
+    let b = 18;
+    println!("gcd({},{})={}", a, b, gcd(a, b));
+
+    let mut numbers = Vec::new();
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
