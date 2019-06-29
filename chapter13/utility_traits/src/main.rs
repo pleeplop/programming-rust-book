ed5c261d4d9a589931d4b3d97eb36b490a408d35
pleeplop <pleeplop@users.noreply.github.com>
2019-06-29 09:14:16 +0200

    chapter13 part2

--- chapter13/utility_traits/src/main.rs
+++ chapter13/utility_traits/src/main.rs
@@ -2,8 +2,11 @@
 #![allow(unused_assignments)]
 #![allow(dead_code)]
 
+use std::borrow::Cow;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
 use std::ops::{Deref, DerefMut, Index};
+use std::path::Path;
 
 use rand;
 
@@ -11,6 +14,7 @@ fn main() {
     main_rc_box();
     main_clone();
     main_deref();
+    main_default();
 }
 
 struct Appellation {
@@ -119,4 +123,69 @@ fn main_deref() {
 
 fn show_it<T: Display>(thing: T) {
     println!("{}", thing);
+}
+
+fn main_default() {
+    let squares = [4, 9, 16, 25, 36, 49, 64];
+    let (powers_of_two, impure): (HashSet<i32>, HashSet<i32>)
+        = squares.iter().partition(|&n| n & (n - 1) == 0);
+    assert_eq!(powers_of_two.len(), 3);
+    assert_eq!(impure.len(), 4);
+
+    let (upper, lower): (String, String)
+        = "Great Teacher Onizuka".chars().partition(|&c| c.is_uppercase());
+    assert_eq!(upper, "GTO");
+    assert_eq!(lower, "reat eacher nizuka");
+}
+
+fn main_as_ref() {
+    let dot_emacs = std::fs::File::open("~/.emacs");
+}
+
+fn main_borrow() {
+    let mut map = HashMap::new();
+    let k1 = "k1".to_string();
+    map.insert(k1, 1);
+    let x = "k1";
+    let v1 = map.get(x);
+}
+
+fn main_from_into() {
+    use std::net::Ipv4Addr;
+    fn ping<A>(address: A) -> std::io::Result<bool>
+        where A: Into<Ipv4Addr> {
+        let ipv4: Ipv4Addr = address.into();
+        unimplemented!();
+    }
+    println!("{:?}", ping(Ipv4Addr::new(23, 21, 68, 141)));
+    println!("{:?}", ping([23, 21, 68, 141]));
+    println!("{:?}", ping(0xd076eb94_u32));
+
+    let addr1 = Ipv4Addr::from([23, 21, 68, 141]);
+    let addr2 = Ipv4Addr::from(0xd076eb94_u32);
+
+    let heap_allocated = String::from("static str");
+}
+
+fn main_to_owned() {
+    let str = "str static";
+    let string = str.to_owned();
+
+    let slice = [1, 2, 3];
+    let vec = slice.to_owned();
+}
+
+fn main_cow() {
+    enum Error {
+        StackOverflow,
+        FileNotFound(&'static Path),
+    }
+
+    use std::path::PathBuf;
+    fn describe(error: &Error) -> Cow<'static, str> {
+        match error {
+            Error::StackOverflow => "out of memory".into(),
+            Error::FileNotFound(ref path) => format!("file not found: {}", path.display()).into(),
+        }
+    }
 }
